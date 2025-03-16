2fc61d954b4148950115a37d7dd18c37bb8a8e0d
Youness SBAI <youness.sbai.work@gmail.com>
2025-03-16 03:53:14 +0000

    Auth setup (Clerk)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,22 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"
 import { Button } from "@/components/ui/button"
 
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen space-y-4">
-      <h1 className="text-4xl font-bold text-teal-600 underline decoration-black">
-        MelodyMosaic
-      </h1>
-      <Button asChild>
-        <a 
-          href="https://github.com/SBAI-Youness"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="bg-teal-500 text-white px-4 py-2 rounded-md hover:bg-teal-600 transition"
-        >
-          Check my GitHub
-        </a>
-      </Button>
-    </div>
+    <>
+      <header>
+        <SignedOut>
+          <SignInButton>
+            <Button>
+              Sign In
+            </Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+    </>
   )
 }
 
