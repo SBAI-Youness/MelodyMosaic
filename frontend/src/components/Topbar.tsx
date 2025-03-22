c5280b0890456fbeb5483a1cfd45305f1eba18ff
Youness SBAI <youness.sbai.work@gmail.com>
2025-03-22 14:49:40 +0000

    Created the home page

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,27 +1,31 @@
-import { SignedIn, SignedOut, SignOutButton, UserButton } from "@clerk/clerk-react"
+import { SignedOut, UserButton } from "@clerk/clerk-react"
 import { LayoutDashboardIcon } from "lucide-react"
 import { Link } from "react-router-dom"
 import SignInOAuthButtons from "./SignInOAuthButtons"
+import { useAuthStore } from "@/stores/useAuthStore"
+import { buttonVariants } from "./ui/button"
+import { cn } from "@/lib/utils"
 
 const Topbar = () => {
-  const isAdmin = false
+  const isAdmin = useAuthStore((state) => state.isAdmin)
 
   return (
     <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/56 backdrop-blur-md z-10">
       <div className="flex gap-2 items-center">
+        <img src="/logo.png" alt="MelodyMosaic logo" className="size-8" />
         MelodyMosaic
       </div>
       <div className="flex items-center gap-4">
-        { isAdmin && (
-          <Link to={"/admin"}>
+        {isAdmin && (
+          <Link to={"/admin"}
+                className={cn(
+                  buttonVariants({variant: "outline"})
+                )}
+            >
             <LayoutDashboardIcon className="size-4 mr-2" />
             Admin Dashboard
           </Link>
         )}
-
-        <SignedIn>
-          <SignOutButton />
-        </SignedIn>
 
         <SignedOut>
           <SignInOAuthButtons />
