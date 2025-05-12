ec53353dd98d1dd0f4aaaa034ad2eb563d68e102
Youness SBAI <youness.sbai.work@gmail.com>
2025-05-12 02:32:44 +0100

    Ready for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,6 +9,8 @@ import path from "path"
 
 import cors from "cors"
 
+import fs from "fs"
+
 import { connectDB } from "./lib/db.js"
 
 import userRoutes from "./routes/user.route.js"
@@ -19,6 +21,8 @@ import albumRoutes from "./routes/album.route.js"
 import statRoutes from "./routes/stat.route.js"
 import { createServer } from "http"
 import { initializeSocket } from "./lib/socket.js"
+
+import cron from "node-cron"
 
 dotenv.config()
 
@@ -45,12 +49,35 @@ app.use(fileUpload({
   },
 }))
 
+// cron job: delete the tmp files every 1 hour
+const tempDir = path.join(process.cwd(), "tmp")
+cron.schedule("0 * * * *", () => {
+	if (fs.existsSync(tempDir)) {
+		fs.readdir(tempDir, (err, files) => {
+			if (err) {
+				console.log("error", err)
+				return;
+			}
+			for (const file of files) {
+				fs.unlink(path.join(tempDir, file), (err) => {})
+			}
+		})
+	}
+})
+
 app.use("/api/users", userRoutes)
 app.use("/api/admin", adminRoutes)
 app.use("/api/auth", authRoutes)
 app.use("/api/songs", songRoutes)
 app.use("/api/albums", albumRoutes)
 app.use("/api/stats", statRoutes)
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../frontend/dist")))
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
+	})
+}
 
 app.use((err, req, res, next) => {
   res.status(500).json({message: process.env.NODE_ENV == "production"? "Internal server error": err.message})
